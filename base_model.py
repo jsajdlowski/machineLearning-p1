e5de1c46df059efe2dd3e52ce2686c8a2f846f1f
Julia Sajdlowski <juliasajdlowski@gmail.com>
2024-05-23 01:40:17 +0200

    add: base models

---
+++ base_model.py
@@ -0,0 +1,23 @@
+from abc import abstractmethod
+
+
+class BaseModel:
+    def __init__(self):
+        pass
+
+    @abstractmethod
+    def fit(self, X):
+        pass
+
+    @abstractmethod
+    def predict(self, X):
+        pass
+
+class DeepBaseModel(BaseModel):
+    def __init__(self):
+        super().__init__()
+        self.instantiate_model()
+
+    @abstractmethod
+    def instantiate_model(self, X):
+        pass
